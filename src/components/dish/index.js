b09341da923569974d09d92044b06e02b55f65ca
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-09-05 19:34:14 +0300

    add custom hook

--- src/components/dish/index.js
+++ src/components/dish/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Button, Card, Typography } from "antd";
+import useAmount from "../../hooks/use-amount";
 
 function Dish({ dish }) {
+  const { amount, increment, decrement } = useAmount();
+
   return (
     <Card title={dish.name}>
       <Typography>{dish.ingredients.join(", ")}</Typography>
-      <Typography.Text strong>0</Typography.Text>
-      <Button>+</Button>
-      <Button>-</Button>
+      <Typography.Text strong>{amount}</Typography.Text>
+      <Button onClick={increment}>+</Button>
+      <Button onClick={decrement}>-</Button>
     </Card>
   );
 }
---
+++ src/hooks/use-amount.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function useAmount(initialAmount = 0) {
+  const [state, setState] = useState(initialAmount);
+
+  const increment = () => setState(state + 1);
+  const decrement = () => state >= 1 && setState(state - 1);
+
+  return { amount: state, increment, decrement };
+}
