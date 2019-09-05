05ddb0c7ef6942526414e2971c9a56bfe31ec535
Aleksandr Volkov <alex@aheadautomatics.com>
2019-09-06 02:53:57 +0300

    HT1.2 Add review form

--- src/components/simple-form/index.js
+++ src/components/simple-form/index.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
+import { Button, Input, Rate } from "antd";
 
 function SimpleForm(props) {
+  const [rate, setRate] = useState(null);
   const [review, setReview] = useState("");
+  const { TextArea } = Input;
+  const handleSubmit = ev => {
+    ev.preventDefault();
+    console.log("form", { rate, review });
+  };
   return (
-    <div>
-      add your review:{" "}
-      <input value={review} onChange={ev => setReview(ev.target.value)} />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <Rate value={rate} onChange={ev => setRate(ev.valueOf())} />
+      <TextArea
+        value={review}
+        placeholder="add your review"
+        rows={4}
+        onChange={ev => setReview(ev.target.value)}
+      />
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </form>
   );
 }
 
