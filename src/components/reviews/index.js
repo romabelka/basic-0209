0fbdbe383a723f75c4b3f2f3aaacb33dbd6bf07a
Aleksandr Volkov <alex@aheadautomatics.com>
2019-09-06 02:35:25 +0300

    HT1.1 Add review list

--- src/components/reviews/index.js
+++ src/components/reviews/index.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Review from "../review";
 import SimpleForm from "../simple-form";
+import { List } from "antd";
 
-function Reviews(props) {
+function Reviews({ reviews }) {
   return (
     <div>
       <h1>Reviews</h1>
+      <List
+        dataSource={reviews}
+        renderItem={review => (
+          <List.Item>
+            <Review review={review} />
+          </List.Item>
+        )}
+        rowKey={review => review.id}
+      />
+      <h2>Post a review</h2>
       <SimpleForm />
     </div>
   );
