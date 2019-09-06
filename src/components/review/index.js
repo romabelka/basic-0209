1509ce80eb09933c8a74e9184121e3a00b286bb9
ermo4enkov <ermo4enkov@gmail.com>
2019-09-07 00:12:40 +0300

    created a form

--- src/components/review/index.js
+++ src/components/review/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Button, Card, Typography, Rate } from "antd";
+import { Card, Typography, Rate } from "antd";
 
 function Review({ review }) {
   return (
     <Card title={review.user}>
       <Typography.Text strong>{review.text}</Typography.Text>
       <div>
-        <Rate value={review.rating} />
+        <Rate value={review.rating} disabled={true} />
       </div>
     </Card>
   );
--- src/components/simple-form/index.js
+++ src/components/simple-form/index.js
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
+import { Typography, Rate, Button, Form } from "antd";
 
 function SimpleForm(props) {
+  const [name, setName] = useState("");
   const [review, setReview] = useState("");
+  const [rate, setRate] = useState(0);
   return (
-    <div>
-      add your review:{" "}
-      <input value={review} onChange={ev => setReview(ev.target.value)} />
-    </div>
+    <Form
+      onSubmit={e => {
+        e.preventDefault();
+        console.log("submit");
+      }}
+    >
+      <Typography.Title level={2}>Add your reivew</Typography.Title>
+      <div>
+        <label htmlFor="">Your name:</label>
+        <input value={name} onChange={ev => setName(ev.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="">Your review:</label>
+        <input value={review} onChange={ev => setReview(ev.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="">Your rate:</label>
+        <Rate value={rate} />
+      </div>
+      <Button type="primary" htmlType={"submit"}>
+        Send
+      </Button>
+    </Form>
   );
 }
 
