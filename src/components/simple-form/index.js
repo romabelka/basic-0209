3626d858cd7ce6fd8806a903522a65b7859c5c39
VladyslavKashuba <skasweetpoison@mail.ru>
2019-09-08 18:27:50 +0300

    Kashuba's first homework, maybe need add decorator for rate component

--- src/components/simple-form/index.js
+++ src/components/simple-form/index.js
@@ -1,11 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
+import { Typography, Form, Row, Col, Input, Rate, Button, message } from "antd";
+import {
+  useValidateRateForm,
+  useValidateMinLength,
+  useRateValue
+} from "../../hooks/use-validate-rate-form";
 
-function SimpleForm(props) {
-  const [review, setReview] = useState("");
+function SimpleForm() {
+  const { setInputReviewText, reviewText } = useValidateRateForm();
+  const { validate, isValid } = useValidateMinLength();
+  const { setRateValue, rateValue } = useRateValue();
+
+  function onChangeTextArea(event) {
+    const { value } = event.target;
+    setInputReviewText(value);
+    validate(value);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    message.success(
+      `Success! Review text: ${reviewText} + Review rate: ${rateValue}`
+    );
+  }
+
+  function onChangeRate(value) {
+    setRateValue(value);
+  }
+
   return (
     <div>
-      add your review:{" "}
-      <input value={review} onChange={ev => setReview(ev.target.value)} />
+      <Row>
+        <Col span={12} offset={6}>
+          <Typography.Title level={4}>add your review</Typography.Title>
+          <Form layout="vertical" onSubmit={onSubmit}>
+            <Form.Item
+              validateStatus={isValid ? "" : "error"}
+              help={isValid ? "" : "Should be min length 5 letters"}
+            >
+              <Input.TextArea
+                rows={4}
+                value={reviewText}
+                onChange={onChangeTextArea}
+              />
+
+              <Rate onChange={onChangeRate} value={rateValue} />
+              <br />
+              <Button htmlType="submit" disabled={!isValid}>
+                Send
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </div>
   );
 }
