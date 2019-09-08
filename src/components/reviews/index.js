4a5317979b66c7c9ae3199873dd25b39513accc4
TommyDBrown <gurinevgeniy@gmail.com>
2019-09-08 23:09:32 +0700

    My first homework

--- src/components/reviews/index.js
+++ src/components/reviews/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import SimpleForm from "../simple-form";
+import Review from "../review";
+import { Row, Col } from "antd";
 
-function Reviews(props) {
+function Reviews({ reviews }) {
   return (
     <div>
       <h1>Reviews</h1>
+      <Row style={{margin: '0 0 30px 0'}}>
+        {reviews.map(review => (
+          <Col span={12} key={review.id} style={{paddingRight: '15px',paddingLeft: '15px'}}>
+            <Review feedback={review} />
+          </Col>
+        ))}
+      </Row>
       <SimpleForm />
     </div>
   );
--- src/components/simple-form/index.js
+++ src/components/simple-form/index.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
+import { Row, Col, Typography, Button, Input, Rate } from "antd";
 
 function SimpleForm(props) {
   const [review, setReview] = useState("");
+  const [rate, setRate] = useState();
+
   return (
-    <div>
-      add your review:{" "}
-      <input value={review} onChange={ev => setReview(ev.target.value)} />
-    </div>
+    <Fragment>
+      <Row>
+        <Col span={24} style={{paddingRight: '15px',paddingLeft: '15px'}}>
+          <Typography.Text strong style={{display: 'block',marginBottom: '10px'}}>Add your review:</Typography.Text>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={16} style={{paddingRight: '15px',paddingLeft: '15px'}}>
+          <Input.TextArea placeholder="Your text" rows={4} value={review} onChange={ev => setReview(ev.target.value)} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} style={{padding: '5px 15px'}}>
+          <Rate onChange={value => setRate(value)} defaultValue={0} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} style={{padding: '5px 15px'}}>
+          <Button size="large" type="primary" onClick={() => (console.log(`Your text is ${review} and your rate ${rate}`))}>Send</Button>
+        </Col>
+      </Row>
+    </Fragment>
   );
 }
 
