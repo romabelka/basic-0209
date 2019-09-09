56c5ee3b178f4253b94913877dfca07ba17a403a
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-09-09 19:20:50 +0300

    add componentDidCatch

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Product from "../product";
 import * as PropTypes from "prop-types";
+import { Typography } from "antd";
 
-function Menu({ menu }) {
-  return (
-    <div>
-      {menu.map(product => (
-        <Product product={product} key={product.id} />
-      ))}
-    </div>
-  );
+class Menu extends React.Component {
+  state = {
+    error: null
+  };
+
+  componentDidCatch(error) {
+    this.setState({ error });
+  }
+
+  render() {
+    if (this.state.error)
+      return (
+        <Typography.Title level={2}>
+          {this.state.error.message}
+        </Typography.Title>
+      );
+    return (
+      <div>
+        {this.props.menu.map(product => (
+          <Product product={product} key={product.id} />
+        ))}
+      </div>
+    );
+  }
 }
 
 Menu.propTypes = {
