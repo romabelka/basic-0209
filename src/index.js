f2c1f60385670b942b6897adf9fd2ed291e331df
agorelovskii <a.gorelovskii@mail.ru>
2019-09-13 13:23:53 +0300

    tabs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { Tabs } from "antd";
 import Restaurant from "./components/restaurant";
 import { restaurants } from "./fixtures";
 import "./index.css";
@@ -8,7 +9,13 @@ import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <Restaurant restaurant={restaurants[0]} />
+    <Tabs defaultActiveKey="1">
+      {restaurants.map(restaurant => (
+        <Tabs.TabPane tab={restaurant.name} key={restaurant.id}>
+          <Restaurant restaurant={restaurant} />
+        </Tabs.TabPane>
+      ))}
+    </Tabs>
   </Provider>,
   document.getElementById("root")
 );
