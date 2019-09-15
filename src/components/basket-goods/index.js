a15ad158724f1e215dd7a50d27a3e7c96828fc87
Valeriia Maltseva <lerok.maltseva96@gmail.com>
2019-09-15 23:50:15 +0300

    added realization of basket

--- src/components/basket-goods/index.js
+++ src/components/basket-goods/index.js
@@ -1,14 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { restaurants } from "../../fixtures";
 
-function BasketGoods() {
+function BasketGoods({ order }) {
+  const orders = Object.keys(order);
+  let products = getProduct();
+
+  function getProduct() {
+    const products = [];
+    restaurants.forEach(function(restaurant) {
+      restaurant.menu.forEach(function(product) {
+        if (orders.includes(product.id)) {
+          products.push({ count: order[product.id], product });
+        }
+      });
+    });
+
+    return products;
+  }
+
   return (
     <div>
       <h1>Корзина:</h1>
+      <div>
+        {products.map(({ count, product }) => (
+          <div key={product.id}>
+            <div>
+              {product.name} ({count})
+            </div>
+            <div>{product.price}</div>
+            <div>total: {count * product.price}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 BasketGoods.propTypes = {};
 
-export default BasketGoods;
+const mapStateToProps = storeState => ({
+  order: storeState.order
+});
+
+export default connect(mapStateToProps)(BasketGoods);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import { Provider } from "react-redux";
 import Restaurant from "./components/restaurant";
 import BasketGoods from "./components/basket-goods";
 import { restaurants } from "./fixtures";
+import { Tabs } from "antd";
 import "./index.css";
 import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BasketGoods />
-    <Restaurant restaurant={restaurants[0]} />
+    <Tabs>
+      {restaurants.map(item => (
+        <Tabs.TabPane key={item.id} tab={item.name}>
+          <Restaurant restaurant={item} />
+        </Tabs.TabPane>
+      ))}
+    </Tabs>
   </Provider>,
   document.getElementById("root")
 );
