8e6938ae7abde3e2cad238accfb4863c027c444c
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-09-23 19:37:35 +0300

    add NotFoundPage

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,21 +1,28 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
 import React from "react";
 
 import Header from "./header";
 import Content from "../content";
 import Basket from "../basket";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Layout>
       <Header />
       <main role="main">
-        <Route path="/checkout" component={Basket} />
-        <Route path="/restaurants/:id" component={Content} />
+        <Switch>
+          <Route path="/checkout" exact component={Basket} />
+          <Route path="/restaurants/:id" component={Content} />
+          <Route path="/" component={NotFound} />
+        </Switch>
       </main>
     </Layout>
   );
+}
+
+function NotFound() {
+  return <Typography.Title level={1}>Not Found Page</Typography.Title>;
 }
 
 export default App;
