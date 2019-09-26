42de91017013fa2f0c939279e8a246ebf674132e
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-09-26 19:13:57 +0300

    add animation example

--- src/components/basket/index.js
+++ src/components/basket/index.js
@@ -3,6 +3,7 @@ import cx from "classnames";
 import React from "react";
 import { Typography } from "antd";
 import { Link } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import styles from "./basket.module.css";
 import BasketRow from "./basket-row";
@@ -12,6 +13,7 @@ import {
   orderedProductsSelector,
   totalPriceSelector
 } from "../../redux/selectors";
+import "./basket.css";
 
 function Basket({ title = "Basket", className, total, orderProducts }) {
   console.log("---", "rendering Basket");
@@ -20,14 +22,18 @@ function Basket({ title = "Basket", className, total, orderProducts }) {
       <Typography.Title level={4} className={styles.title}>
         {title}
       </Typography.Title>
-      {orderProducts.map(({ product, amount, restaurant }) => (
-        <BasketItem
-          product={product}
-          amount={amount}
-          restaurant={restaurant}
-          key={product.id}
-        />
-      ))}
+      <TransitionGroup>
+        {orderProducts.map(({ product, amount, restaurant }) => (
+          <CSSTransition timeout={500} classNames="basket-item-animation">
+            <BasketItem
+              product={product}
+              amount={amount}
+              restaurant={restaurant}
+              key={product.id}
+            />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
       <hr />
 
       <BasketRow leftContent="Sub-total" rightContent={`${total} $`} />
