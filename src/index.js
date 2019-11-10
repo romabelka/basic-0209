fa80dd12bf1dde336fb0b0c6ea819ac748a67c85
Valeriia Maltseva <lerok.maltseva96@gmail.com>
2019-11-10 20:20:21 +0200

    changed project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import Restaurant from "./components/restaurant";
-import BasketGoods from "./components/basket-goods";
 import { restaurants } from "./fixtures";
-import { Tabs } from "antd";
-import "./index.css";
-import store from "./redux/store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BasketGoods />
-    <Tabs>
-      {restaurants.map(item => (
-        <Tabs.TabPane key={item.id} tab={item.name}>
-          <Restaurant restaurant={item} />
-        </Tabs.TabPane>
-      ))}
-    </Tabs>
-  </Provider>,
-  document.getElementById("root")
-);
+import "./index.css";
+
+ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
